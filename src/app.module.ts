1f75d818fa37a6d6140389f4068a8c92ccbdafb6
Kevin Wang <aoling.wang@gmail.com>
2026-08-22 13:28:59 -0400

    Update app.module.ts
    Updating to remove app-wide controller and service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { MedicationsModule } from './medications/medications.module';
 
 @Module({
   imports: [MedicationsModule],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
