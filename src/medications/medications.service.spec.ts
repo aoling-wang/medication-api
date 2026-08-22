55153a6968fe112d3da0bbd9895063ba973e95b9
Kevin Wang <aoling.wang@gmail.com>
2026-08-22 12:53:43 -0400

    Update medications.service.spec.ts
    Adding tests for medications service

--- src/medications/medications.service.spec.ts
+++ src/medications/medications.service.spec.ts
@@ -5,14 +5,26 @@ describe('MedicationsService', () => {
   let service: MedicationsService;
 
   beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [MedicationsService],
-    }).compile();
+    const module: TestingModule =
+      await Test.createTestingModule({
+        providers: [MedicationsService],
+      }).compile();
 
-    service = module.get<MedicationsService>(MedicationsService);
+    service = module.get<MedicationsService>(
+      MedicationsService,
+    );
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+  it('should return medications', () => {
+    const medications = service.findAll();
+
+    expect(medications.length).toBeGreaterThan(0);
+  });
+
+  it('should find a medication by id', () => {
+    const medication = service.findOne(1);
+
+    expect(medication).toBeDefined();
+    expect(medication?.name).toBe('Metformin');
   });
 });
