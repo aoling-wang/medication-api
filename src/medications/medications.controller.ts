ee70f07f1f26d9c92872c727326799fa2ef41aff
Kevin Wang <aoling.wang@gmail.com>
2026-08-22 12:05:08 -0400

    Update medications.controller.ts

--- src/medications/medications.controller.ts
+++ src/medications/medications.controller.ts
@@ -1,4 +1,38 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, ParseIntPipe, Post } from '@nestjs/common';
+import { MedicationsService } from './medications.service';
+import type { Medication } from './interfaces/medication.interface';
 
 @Controller('medications')
-export class MedicationsController {}
+export class MedicationsController {
+    constructor(
+        private readonly medicationsService: MedicationsService,
+    ) {}
+
+    @Get()
+    findAll(): Medication[] {
+        return this.medicationsService.findAll();
+    }
+
+    @Get(':id')
+    findOne(
+        @Param('id', ParseIntPipe) id: number,
+    ): Medication | undefined {
+        return this.medicationsService.findOne(id);
+    }
+
+    @Post()
+    create(
+        @Body() medication: Omit<Medication, 'id'>,
+    ): Medication {
+        return this.medicationsService.create(medication);
+    }
+
+    @Delete(':id')
+    remove(
+        @Param('id', ParseIntPipe) id: number,
+    ): { deleted: boolean } {
+        return {
+        deleted: this.medicationsService.remove(id),
+        };
+    }
+}
